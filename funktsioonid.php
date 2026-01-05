87f3d787e7d6b8ddbead14db3b7be0c42d9b62ec
Martin Rossakov <mstislavlandsh@gmail.com>
2026-01-05 12:57:49 +0200

    lisa punkt funktsioon on täiendatud

--- funktsioonid.php
+++ funktsioonid.php
@@ -8,7 +8,6 @@ function lisapunkt($id){
         $paring = $yhendus->prepare("UPDATE valimised SET punktid = punktid + 1 WHERE id = ?");
         $paring->bind_param("i", $id);
         $paring->execute();
-        header("Location: " . $_SERVER['PHP_SELF']); // aadressiriba puhastab päring ja jääb failinimi
         $yhendus->close();
 
 }
