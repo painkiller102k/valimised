7b2a91049269825c2f15a91070a337c6aaab1b43
Martin Rossakov <mstislavlandsh@gmail.com>
2026-01-05 13:05:15 +0200

    naita tabel funktsioon on loodud

--- funktsioonid.php
+++ funktsioonid.php
@@ -11,3 +11,19 @@ function lisapunkt($id){
         $yhendus->close();
 
 }
+
+function naitatabel()
+{
+    global $yhendus;
+    $paring = $yhendus->prepare("Select id, president, pilt, punktid, lisamisaeg, kommentaarid from valimised where avalik=1");
+    $paring->bind_result($id, $president, $pilt, $punktid, $lisamisaeg, $kommentaarid);
+    $paring->execute();
+    while($paring->fetch()) {
+        echo "<tr>";
+        echo "<td>{$president}</td>";
+        echo "<td>{$punktid}</td>";
+        echo "<td><a href='?lisa1punkt={$id}'> +1 punkt</a></td>";
+        echo "<td><a href='?minus1punkt={$id}'> -1 punkt</a></td>";
+        echo "</tr>";
+        }
+}
